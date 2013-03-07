2049ec31bcf832d23d9c67b910e87f688dd4f6d6
Makara Wang <makara15@gmail.com>
2013-03-07 14:14:29 +0800

    Added seed.

--- test/carcass.mocha.js
+++ test/carcass.mocha.js
@@ -1,73 +1,39 @@
 var carcass = require('carcass');
 var should = require('should');
 
-require('./fixture');
-
 describe('Carcass', function() {
-    it('should be an object with some methods.', function(done) {
+    it('should be an object with some methods.', function() {
         carcass.should.be.a('object');
         carcass.should.have.property('register');
         carcass.should.have.property('mixable');
-        done();
+        carcass.should.have.property('deferred');
+        carcass.should.have.property('promise');
+        carcass.should.have.property('seed');
     });
 
     // TODO
-    it('should have some utils.', function(done) {
+    it('should have some utils.', function() {
         carcass.should.have.property('utils');
-        done();
     });
 
     // TODO
-    it('should have some plugins.', function(done) {
+    it('should have some plugins.', function() {
         carcass.should.have.property('plugins');
-        done();
     });
 
     // TODO
-    it('should have some constructors.', function(done) {
+    it('should have some constructors.', function() {
         carcass.should.have.property('constructors');
-        done();
     });
 
     // TODO
-    it('should have some factories.', function(done) {
+    it('should have some factories.', function() {
         carcass.should.have.property('factories');
-        done();
     });
 
     // TODO
-    it('should have some applications.', function(done) {
+    it('should have some applications.', function() {
         carcass.should.have.property('applications');
-        done();
-    });
-
-    // .
-    describe('Mixin', function() {
-        it('should be able to merge objects.', function(done) {
-            var obj = carcass.mixable();
-            var value = false;
-            var mixinObj = {
-                lorem: function() {
-                    this.should.equal(obj);
-                    value = true;
-                    return 'lorem';
-                }
-            };
-            obj.mixin(mixinObj);
-            obj.should.have.property('lorem');
-            obj.lorem().should.equal('lorem');
-            value.should.equal(true);
-            done();
-        });
-    });
-
-    describe('Register', function() {
-        it('should be able to register new files.', function(done) {
-            carcass.register('', 'applications');
-            carcass.applications.should.have.property('ipsum');
-            carcass.applications.should.have.property('lorem');
-            done();
-        });
     });
 
     // TODO: more.
